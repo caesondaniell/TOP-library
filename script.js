410a745ba5bf747856da9bb7f421fcc489db4e50
Caeson Daniell <239234311+caesondaniell@users.noreply.github.com>
2026-03-24 10:53:27 -0700

    Update form validation
    Future commits:
    |Features:
    |-none
    |
    |Fixes:
    |-none
    |
    |Other:
    |-none

--- script.js
+++ script.js
@@ -1,99 +1,109 @@
-const dialog = document.querySelector("dialog");
-const buttons = document.querySelectorAll("button");
-const library = [];
+const dialog = document.querySelector("dialog")
+const title = dialog.querySelector("#title")
+const author = dialog.querySelector("#author")
+const pages = dialog.querySelector("#pgs")
+const state = dialog.querySelector("#status")
+const buttons = document.querySelectorAll("button")
+const library = []
+
 class Book {
-  constructor(title, author, pages, readState) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.readState = readState;
+    constructor (title, author, pages, readState) {
+        this.title = title,
+        this.author = author,
+        this.pages = pages,
+        this.readState = readState
     }
     
-    id = crypto.randomUUID();
+    id = crypto.randomUUID()
     
     addToLibrary() {
-        let exists;
+        let exists
         library.forEach(book => {
             if (book.title === this.title && book.author === this.author) {
-                exists = 1;
+                exists = 1
             }
-        });
+        })
         if (exists) {
-            alert("That book is already in your library!");
-        } else library.push(this);
+            alert("That book is already in your library!")
+        } else library.push(this)
     }
     
     changeState(state) {
-        this.readState = state;
+        this.readState = state
     }
 }
+
+title.addEventListener("blur", (e) => checkField(e.target))
+author.addEventListener("blur", (e) => checkField(e.target))
+state.addEventListener("blur", (e) => checkField(e.target))
 
 buttons.forEach(button => {
     button.addEventListener("click", (e) => {
         switch (button.classList[0]) {
             case "add":
-                dialog.showModal();
-                break;
+                dialog.showModal()
+                title.focus()
+                break
             case "close":
-                dialog.close();
-                break;
+                dialog.close()
+                break
             case "submit":
-                let title = dialog.querySelector("#title");
-                let author = dialog.querySelector("#author");
-                let pages = dialog.querySelector("#pgs");
-                let status = dialog.querySelector("#status");
-                if (title.value !== "" && 
-                    author.value !== "" && 
-                    status.value !== "") {
-                        e.preventDefault();
-                        const book = new Book(title.value, author.value, pages.value, status.value);
-                        title.value = "";
-                        author.value = "";
-                        pages.value = "";
-                        status.value = "";
-                        dialog.close();
-                        book.addToLibrary();
-                        updateLibrary();
-                };
-                break;
+                const form = document.querySelector('form')
+                if (form.checkValidity()) {
+                    e.preventDefault()
+                    const book = new Book(
+                        title.value,
+                        author.value,
+                        pages.value,
+                        state.value
+                    )
+                    title.value = ""
+                    author.value = ""
+                    pages.value = ""
+                    state.value = ""
+                    dialog.close()
+                    book.addToLibrary()
+                    updateLibrary()
+                }
+                break
         }
     })
-});
+})
 
 function updateLibrary() {
-    const bookList = document.querySelector("tbody");
-    const presentTitles = bookList.querySelectorAll(".title");
-    const titleList = [];
-    const presentAuthors = bookList.querySelectorAll(".author");
-    const authorList = [];
+    const bookList = document.querySelector("tbody")
+    const presentTitles = bookList.querySelectorAll(".title")
+    const titleList = []
+    const presentAuthors = bookList.querySelectorAll(".author")
+    const authorList = []
     for (let i = 0; i < presentTitles.length; i++) {
-        titleList.push(presentTitles[i].textContent);
+        titleList.push(presentTitles[i].textContent)
     }
     for (let i = 0; i < presentAuthors.length; i++) {
-        authorList.push(presentAuthors[i].textContent);
+        authorList.push(presentAuthors[i].textContent)
     }
     library.forEach(book => {
         if (titleList.includes(book.title) &&
             authorList.includes(book.author) &&
             titleList.indexOf(book.title) === authorList.indexOf(book.author)) {
-                titleList.splice(0,1);
-                authorList.splice(0,1);
-                return;
+                titleList.splice(0,1)
+                authorList.splice(0,1)
+                return
         } else {
-            const row = document.createElement("tr");
-            row.setAttribute("data-id", book.id);
-            bookList.appendChild(row);
+            const row = document.createElement("tr")
+            row.setAttribute("data-id", book.id)
+            bookList.appendChild(row)
             for (const detail in book) {
                 if (!(detail === "id")) {
-                    const cell = document.createElement("td");
-                    cell.classList.add(`${detail}`);
+                    const cell = document.createElement("td")
+                    cell.classList.add(`${detail}`)
                     if (!(detail === "readState")) {
-                        cell.textContent = book[detail];
+                        cell.textContent = book[detail]
                     } else {
-                        const statusSelector = document.createElement("select");
-                        statusSelector.setAttribute("name", `${book.id}-status`);
-                        statusSelector.setAttribute("id", `${book.id}-status`);
-                        cell.appendChild(statusSelector);
+                        const statusSelector = document.createElement("select")
+                        statusSelector.setAttribute("name", `${book.id}-status`)
+                        statusSelector.setAttribute("id", `${book.id}-status`)
+                        cell.appendChild(statusSelector)
                         const optionList = [
                             "want to read", 
                             "reading", 
@@ -101,18 +111,19 @@ function updateLibrary() {
                             "finished"
                         ]
                         optionList.forEach(option => {
-                            const statusOption = document.createElement("option");
-                            statusOption.setAttribute("value", option);
-                            statusOption.textContent = option;
-                            statusSelector.appendChild(statusOption);
+                            const statusOption = document
+                                                .createElement("option")
+                            statusOption.setAttribute("value", option)
+                            statusOption.textContent = option
+                            statusSelector.appendChild(statusOption)
                         })
-                        statusSelector.value = book[detail];
+                        statusSelector.value = book[detail]
                         statusSelector.addEventListener("input", () => {
-                            book.changeState(statusSelector.value);
-                            console.log(library);
+                            book.changeState(statusSelector.value)
+                            console.log(library)
                         })
                     }
-                    row.appendChild(cell);
+                    row.appendChild(cell)
                 }
             }
 
@@ -136,7 +147,17 @@ function updateLibrary() {
     // console.log(library);
 }
 
+function checkField (element) {
+    if (element.value === '') {
+        element.classList.add('error')
+        element.setCustomValidity('This is required!')
+    } else {
+        element.classList.remove('error')
+        element.setCustomValidity('')
+    }
+}
+
 // --Here for when I need to add/test/style features--
-// addToLibrary("House of Blades", "Chuckie", 10, "want to read");
-// addToLibrary("Sunshine and Daisies", "Barney", 20, "finished");
-// updateLibrary();
+// addToLibrary("House of Blades", "Chuckie", 10, "want to read")
+// addToLibrary("Sunshine and Daisies", "Barney", 20, "finished")
+// updateLibrary()
